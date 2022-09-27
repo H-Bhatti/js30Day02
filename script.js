883255d9bbce0065040d8564fbd06ca35474b4f0
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2022-09-27 22:01:49 +0500

    final working form of code everythign is working flawlwssly just eed to chane the color of bcakground because why not

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const secHand = document.querySelector(".second-hand")
+setInterval(() =>{
 
-let setDate = () =>{
+
+    let secHand = document.querySelector(".second-hand")
+    let minHand = document.querySelector(".min-hand")
+    let hourHand = document.querySelector(".hour-hand")
     
     let now = new Date()
     let sec = now.getSeconds();
     let secDegree = ((sec/60)*360)+90
 
+    let min = now.getMinutes();
+    let minDegree = ((min/60)*360)+90
+
+    let hour=now.getHours();
+    let hourDegree=((hour/24)*360)+90
+
 
     //transition property on css for seconds
     secHand.style.transform=`rotate(${secDegree}deg)`
-    console.log(sec)
-}
+    // console.log(sec)
 
-setInterval(setDate,1000)
+
+    //transition property on css for min
+    minHand.style.transform=`rotate(${minDegree}deg)`
+    // console.log(min)
+
+    // transition property on css for hour
+    hourHand.style.transform=`rotate(${hourDegree}deg)`
+    // console.log(`hours = ${hour}`)
+
+
+
+},1000)
